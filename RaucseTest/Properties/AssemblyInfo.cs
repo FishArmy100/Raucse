b032668ba811c878a4caaa2591401661cb48701a
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2023-03-10 19:06:18 -0500

    Added a test project, doesnt work

---
+++ RaucseTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RaucseTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RaucseTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6309de92-7806-4691-a88c-4a36d5abf76d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RaucseTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Raucse;
+
+namespace RaucseTest
+{
+	[TestClass]
+	public class UnitTest
+	{
+		[TestMethod]
+		public void TestMethod()
+		{
+			Assert.IsTrue(Test.False(), "I wanted to make sure that this is false.");
+		}
+	}
+}
