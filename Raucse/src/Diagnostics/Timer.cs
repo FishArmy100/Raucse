f37b31dcc10c397c926e32bb5db8d105a4de3e3f
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2023-06-06 18:11:45 -0400

    Added a timer

---
+++ Raucse/src/Diagnostics/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raucse.Diagnostics
+{
+    public static class Timer
+    {
+        public static TimeSpan Time(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+
+            return stopwatch.Elapsed;
+        }
+
+        public static Pair<TimeSpan, T> Time<T>(Func<T> func)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            T value = func();
+            stopwatch.Stop();
+
+            return new Pair<TimeSpan, T>(stopwatch.Elapsed, value);
+        }
+    }
+}
